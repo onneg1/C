a48b4f2c3879706bd83bc5b8ebe635d4cf92b00c
onneg1 <143418613+onneg1@users.noreply.github.com>
2023-10-13 21:58:27 -0300

    COM QUESTAO tabuada com repetição.c

--- tabuada com repetição.c
+++ tabuada com repetição.c
@@ -71,3 +71,71 @@ getchar ();
 return 0;
 
 }
+
+
+
+
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    int n1;
+    int n2;
+    int n3;
+    
+    printf ("Digite o primeiro número inteiro.\n");
+    scanf ("%d", &n1);
+    
+    printf ("Digite o segundo número inteiro. \n");
+    scanf ("%d", &n2);
+    
+    printf ("Digite o terceiro número inteiro. \n");
+    scanf ("%d", &n3);
+    
+    
+    
+
+ 
+    return 0;
+}
+
+
+
+
+
+
+Crie um programa em C que peça ao usuário para inserir três números inteiros e, em seguida, determine e imprima o maior número entre eles. Além disso, o programa deve lidar com casos em que dois ou mais números são iguais.
+
+Aqui está um exemplo de como você pode implementar essa questão:
+
+c
+Copy code
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2, numero3;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &numero1);
+    
+    printf("Digite o segundo número: ");
+    scanf("%d", &numero2);
+
+    printf("Digite o terceiro número: ");
+    scanf("%d", &numero3);
+
+    if (numero1 >= numero2 && numero1 >= numero3) {
+        printf("O maior número é: %d\n", numero1);
+    } else if (numero2 >= numero1 && numero2 >= numero3) {
+        printf("O maior número é: %d\n", numero2);
+    } else {
+        printf("O maior número é: %d\n", numero3);
+    }
+
+    return 0;
+}
+
